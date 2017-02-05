8f79bcc2a6951eeb74f0fde0113ad07b9e826412
cherylyli <cherylliat@gmail.com>
2017-02-04 22:38:04 -0500

    update app.py to tweet as well as retweet, also update keys

--- ObamaBot/app.py
+++ ObamaBot/app.py
@@ -1,6 +1,7 @@
 import os
 import time
 from markovbot import MarkovBot
+from config import Config
 
 tweetbot = MarkovBot()
 
@@ -8,15 +9,16 @@ dirname = os.path.dirname(os.path.abspath(__file__))
 book = os.path.join(dirname, 'input.txt')
 
 tweetbot.read(book)
+passwords = Config()
 
 my_first_text = tweetbot.generate_text(25, seedword=['dream', 'America'])
 print("tweetbot says: ")
 print(my_first_text)
 
-cons_key = "npSslr2RA4y9qezk8jRVjJTPF"
-cons_secret = "FBofbsP78nHJ5KJB0Gyk8Ni7jcCj7t8YJM78wsIXJNxIZ3XoFk"
-access_token = "1580605578-19qlEeAvQyeY1GEr43F09fPg8UBkjSltjhbZ21e"
-access_token_secret = "xUsHipH2OJ1A3o90vH4uD1D2xGU3s2KC9PRDiMhmwGjVa"
+cons_key = passwords.cons_key
+cons_secret = passwords.cons_secret
+access_token = passwords.access_token
+access_token_secret = passwords.access_token_secret
 
 tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
 
@@ -24,12 +26,13 @@ while True:
 	targetString = "DonaldTrump"
 	keywords = []
 
-	prefix = '@realDonaldTrump'
+	prefix = None
 	suffix = "#ObamaBot"
 	maxconvdepth = 5
 
 
 	tweetbot.twitter_autoreply_start(targetString, keywords=keywords, prefix=prefix, suffix=suffix, maxconvdepth=maxconvdepth)
+	tweetbot.twitter_tweeting_start(days=0, hours=0, minutes=30, keywords=None, prefix=None, suffix=suffix)
  
  	time.sleep(100)
  	print("hello")
